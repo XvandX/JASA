900f38334002a5b44acf7061d9795cfe0db986b8
josue martin <biojosue@gmail.com>
2020-05-10 11:30:04 +0200

    aplicación material elementos lista, aplicación elementos material a los textos de cabecera, modificación hover y adaptación tabla lista de canciones

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -14,12 +15,14 @@ import {MatIconModule} from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatListModule
   ],
   exports: [
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatListModule
   ],
 })
 export class MaterialModule { }
